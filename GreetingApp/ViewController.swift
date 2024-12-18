2f8232bae1436553ca8853ee3047941733669432
Maxim Bondarenko <maxim8bondarenko@gmail.com>
2024-12-18 19:02:14 +0300

    Settup UI

--- GreetingApp/ViewController.swift
+++ GreetingApp/ViewController.swift
@@ -9,11 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var greetingLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        greetingLabel.isHidden.toggle()
+        
     }
 
 
+    @IBAction func greetingButtonDidTapped(_ sender: UIButton) {
+    }
 }
-
